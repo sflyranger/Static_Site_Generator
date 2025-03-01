3ea91cec2f75a388c1868205e2d1d32dc3e2faee
Steven Evans <151431504+sflyranger@users.noreply.github.com>
2025-02-28 21:31:05 -0600

    added and finished new split_nodes_delimiter unit tests.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -62,7 +62,33 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(expected_b, repr_bold_nodes)
         self.assertEqual(expected_i, repr_italic_nodes)
 
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
 
+    def test_delim_bold_and_italic(self):
+        node = TextNode("**bold** and _italic_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("bold", TextType.BOLD),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+            ],
+            new_nodes,
+        )
 
 
 
